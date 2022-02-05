35c2ec3c9bc29e43f95493182af490667557cdd6
Lucas Almeida <lucasaclima03@gmail.com>
2022-02-05 12:14:34 -0300

    criação da oferta do dia

--- lib/pages/components/body.dart
+++ lib/pages/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'categories_card.dart';
 import 'header_modal.dart';
+import 'oferta_do_dia.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -11,8 +12,22 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        children: const [HeaderModal(), CategoriesCard()],
+        children: const [HeaderModal(), CategoriesCard(), OfertaDoDia()],
       ),
     );
+  }
+}
+
+class Saia extends StatefulWidget {
+  const Saia({Key? key}) : super(key: key);
+
+  @override
+  _SaiaState createState() => _SaiaState();
+}
+
+class _SaiaState extends State<Saia> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
---
+++ lib/pages/components/oferta_do_dia.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OfertaDoDia extends StatelessWidget {
+  const OfertaDoDia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 400,
+      height: 400,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/oferta_do_dia.jpg'),
+        ),
+      ),
+    );
+  }
+}
